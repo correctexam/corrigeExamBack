ed8a52370065e0f1f31efc1eec54b5b007e46641
Olivier Barais <olivieR.barais@gmail.com>
2022-03-18 14:48:41 +0100

    update maven

--- buildnative.sh
+++ buildnative.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+export GRAALVM_HOME=/opt/graalvm-ce-java11-22.0.0.2
 export JAVA_HOME=${GRAALVM_HOME}
 export PATH=${GRAALVM_HOME}/bin:$PATH
 cp web.xml.prod src/main/resources/META-INF/web.xml
@@ -7,6 +8,6 @@ sleep 10
 ./mvnw compile
 ./mvnw package -Pnative
 cp web.xml.dev src/main/resources/META-INF/web.xml
-docker image rm barais/grade-scope-istic
-docker build -f src/main/docker/Dockerfile.native -t barais/grade-scope-istic .
+sudo docker image rm barais/grade-scope-istic
+sudo docker build -f src/main/docker/Dockerfile.native -t barais/grade-scope-istic .
 
