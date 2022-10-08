cbdb95ec8d0e2fcfa5616b8f1e199919d5347647
Olivier Barais <olivieR.barais@gmail.com>
2022-10-08 14:04:25 +0200

    update MCQ

--- src/main/java/fr/istic/service/QuestionService.java
+++ src/main/java/fr/istic/service/QuestionService.java
@@ -1,7 +1,10 @@
 package fr.istic.service;
 
 import io.quarkus.panache.common.Page;
+import fr.istic.domain.GradedComment;
 import fr.istic.domain.Question;
+import fr.istic.domain.StudentResponse;
+import fr.istic.domain.TextComment;
 import fr.istic.service.dto.QuestionDTO;
 import fr.istic.service.mapper.QuestionMapper;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
@@ -11,9 +14,14 @@ import org.slf4j.LoggerFactory;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 @Transactional
@@ -31,6 +39,74 @@ public class QuestionService {
         question = Question.persistOrUpdate(question);
         return questionMapper.toDto(question);
     }
+
+    @Transactional()
+    public QuestionDTO cleanAllCorrectionAndComment(QuestionDTO questionDTO) {
+        log.debug("Request to clean Question : {}", questionDTO);
+        var question = questionMapper.toEntity(questionDTO);
+        List<GradedComment> gradeComment = new ArrayList<GradedComment>();
+        List<TextComment> textComments = new ArrayList<TextComment>();
+        gradeComment.addAll(GradedComment.findByQuestionId(question.id).list());
+        textComments.addAll(TextComment.findByQuestionId(question.id).list());
+        this.updateCorrectionAndAnswer(question, gradeComment,  textComments);
+        List<Long> gradeCommentids = gradeComment.stream().map(gc -> gc.id).collect(Collectors.toList());
+        List<Long> textCommentsids = textComments.stream().map(gc -> gc.id).collect(Collectors.toList());
+
+        this.deleteComments(gradeCommentids,  textCommentsids);
+
+        return questionMapper.toDto(question);
+    }
+
+    protected void updateCorrectionAndAnswer (Question question,  List<GradedComment> gradeComment , List<TextComment> textComments){
+        Set<StudentResponse> srs = new HashSet(StudentResponse.findAllByQuestionId(question.id).list());
+        for (GradedComment gc : gradeComment) {
+            gc.studentResponses.clear();
+            gc.question = null;
+            gc.persistOrUpdate();
+            List<StudentResponse> srs1 = StudentResponse.findAllByGradedCommentsIds(gc.id).list();
+            srs.addAll(srs1);
+        }
+        for (TextComment gc : textComments) {
+            gc.studentResponses.clear();
+            gc.question = null;
+            gc.persistOrUpdate();
+            List<StudentResponse> srs1 = StudentResponse.findAllByTextCommentsIds(gc.id).list();
+            srs.addAll(srs1);
+        }
+        for (StudentResponse sr : srs) {
+
+            sr.clearCommentsAndNote();
+        }
+
+
+
+        question.gradedcomments.clear();
+        question.textcomments.clear();
+
+        Question.update(question).persistOrUpdate();
+
+
+
+    }
+    @Transactional
+    protected void deleteComments (List<Long> gradeComment , List<Long> textComments){
+
+
+    for (Long gc : gradeComment) {
+        System.err.println("delete gc" + gc);
+        //GradedComment.deleteById(gc.id);
+        GradedComment.deleteById(gc);
+      //  gc.delete();
+    }
+    for (Long gc : textComments) {
+        System.err.println("delete gc" + gc);
+        //GradedComment.deleteById(gc.id);
+        TextComment.deleteById(gc);
+      //  gc.delete();
+    }
+}
+
+
 
     /**
      * Delete the Question by id.
