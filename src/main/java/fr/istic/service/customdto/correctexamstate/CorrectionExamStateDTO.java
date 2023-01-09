814826bd9855e2e585130e3fa8c9c82de7255578
arnobl <1052622+arnobl@users.noreply.github.com>
2023-01-09 17:14:41 +0100

    fix(rest): fixing the getExamStatus route

---
+++ src/main/java/fr/istic/service/customdto/correctexamstate/CorrectionExamStateDTO.java
@@ -0,0 +1,23 @@
+package fr.istic.service.customdto.correctexamstate;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class CorrectionExamStateDTO {
+    private String nameExam;
+    private List<QuestionStateDTO> questions = new ArrayList<QuestionStateDTO>();
+    private List<SheetStateDTO> sheets = new ArrayList<SheetStateDTO>();
+
+    public String getNameExam() { return nameExam; }
+    public void setNameExam(String value) { this.nameExam = value; }
+
+    public List<QuestionStateDTO> getQuestions() { return questions; }
+    public void setQuestions(List<QuestionStateDTO> value) { this.questions = value; }
+
+    public List<SheetStateDTO> getSheets() { return sheets; }
+    public void setSheets(List<SheetStateDTO> value) { this.sheets = value; }
+
+}
