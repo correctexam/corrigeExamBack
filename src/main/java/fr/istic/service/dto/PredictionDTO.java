c1e6090d0efd8d03a5989685592fb65e01968d2b
Pinar Tur <pinar.tur@insa-rennes.fr>
2024-10-30 12:51:32 +0100

    prediction_entity

---
+++ src/main/java/fr/istic/service/dto/PredictionDTO.java
@@ -0,0 +1,55 @@
+package fr.istic.service.dto;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import java.io.Serializable;
+import java.util.Objects;
+import jakarta.persistence.Lob;
+
+/**
+ * A DTO for the {@link fr.istic.domain.Prediction} entity.
+ */
+@RegisterForReflection
+public class PredictionDTO implements Serializable {
+
+    public Long id;
+
+    public String text;
+
+    @Lob
+    public String jsonData;
+
+    public String zonegeneratedid;
+
+    public String questionNumber;
+
+    public Long questionId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PredictionDTO)) {
+            return false;
+        }
+
+        return id != null && id.equals(((PredictionDTO) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return "PredictionDTO{" +
+            "id=" + id +
+            ", text='" + text + "'" +
+            ", jsonData='" + jsonData + "'" +
+            ", zonegeneratedid='" + zonegeneratedid + "'" +
+            ", questionNumber='" + questionNumber + "'" +
+            ", questionId=" + questionId +
+            "}";
+    }
+}
