5d4c4fda52b25d6d4a8957ede1da077193e08f15
Olivier Barais <olivieR.barais@gmail.com>
2025-04-12 08:22:00 +0200

    update

--- src/main/java/fr/istic/service/customdto/EntityId.java
+++ src/main/java/fr/istic/service/customdto/EntityId.java
@@ -1,21 +1,23 @@
 package fr.istic.service.customdto;
 
+import java.util.List;
+
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class EntityId {
 
-    public EntityId(long id){
+    public EntityId(Long id){
         this.id = id;
     }
 
-    long id;
+    Long id;
 
-    public long getId() {
+    public Long  getId() {
         return this.id;
     }
 
-    public void setId(long id) {
+    public void setIds(Long  id) {
         this.id = id;
     }
 }
