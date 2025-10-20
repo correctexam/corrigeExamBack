c038ea7ab583d58760d45e067335d649e3a2a34a
Olivier Barais <olivieR.barais@gmail.com>
2025-10-20 13:57:22 +0200

    remove log

--- src/main/java/fr/istic/service/ZoneService.java
+++ src/main/java/fr/istic/service/ZoneService.java
@@ -54,15 +54,13 @@ public class ZoneService {
      */
     @Transactional
     public void delete(Long id) {
-        log.error("Request to delete Zone : {}", id);
+        log.debug("Request to delete Zone : {}", id);
         Optional<Question> q = Question.findQuestionbyZoneId(id).firstResultOptional();
         if (q.isPresent()) {
             questionSerivce.cleanAllCorrectionAndComment(q.get());
             questionSerivce.delete(q.get().id);
         } else{
             Optional<Exam> exam =   Exam.findExamThatMatchZoneId(id).firstResultOptional();
-
-            log.debug("Exam found to clean zone : {}", exam.get().id);
             if (exam.isPresent()) {
                 if (exam.get().namezone != null && exam.get().namezone.id.equals(id)){
                     Exam.removeNameZoneId(exam.get());
